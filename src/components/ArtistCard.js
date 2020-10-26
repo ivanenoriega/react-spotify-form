ab540f426444364a29fa5cdec1c3eb465ef5bd68
damiloyola <damiloyola@gmail.com>
2020-10-26 20:51:01 -0300

    Creo branch y component ArtistCard

---
+++ src/components/ArtistCard.js
@@ -0,0 +1,21 @@
+function ArtistCard() {
+    return (
+        <li key={i + "_artist"} className="artist">
+            <Heart className="artist__like" />
+            <img className="artist__logo" src={artistLogo} alt="artist logo" />
+            <h2 className="artist__title">Nombre del artista</h2>
+            <p className="artist__followers">
+                Seguidores: <span>9999</span>
+            </p>
+            <div className="artist__stars">
+                <Star className="artist__star" />
+                <Star className="artist__star" />
+                <Star className="artist__star" />
+                <Star className="artist__star" />
+                <Star className="artist__star" />
+            </div>
+        </li>
+    );
+}
+
+export default ArtistCard;
