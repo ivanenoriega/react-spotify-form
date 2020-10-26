01c6fd081ef85186364aabef2aa1ec11b0c19ae4
AgostinaArevaloJaube <agosarevalo@gmail.com>
2020-10-26 20:54:31 -0300

    Agrega select

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ReactComponent as Heart } from "./assets/images/heart-empty.svg";
 import { ReactComponent as Star } from "./assets/images/star-empty.svg";
 import artistLogo from "./assets/images/artist-logo.svg";
 import artistsList from "./helpers/artists";
+import Select from './components/Select'
 import "./App.css";
 
 function App() {
@@ -43,39 +44,11 @@ function App() {
               type="text"
             />
           </li>
-          <li className="filters__item">
-            <select className="filters__input" name="genres">
-              <option value="">Genero</option>
-              <option value="">Genero</option>
-              <option value="">Genero</option>
-              <option value="">Genero</option>
-              <option value="">Genero</option>
-              <option value="">Genero</option>
-            </select>
-          </li>
-          <li className="filters__item">
-            <select className="filters__input" name="popularity">
-              <option value="">Popularidad</option>
-              <option value="">Popularidad</option>
-              <option value="">Popularidad</option>
-              <option value="">Popularidad</option>
-              <option value="">Popularidad</option>
-              <option value="">Popularidad</option>
-              <option value="">Popularidad</option>
-            </select>
-          </li>
-          <li className="filters__item">
-            <select className="filters__input" name="followers">
-              <option value="">Seguidores</option>
-              <option value="">Seguidores</option>
-              <option value="">Seguidores</option>
-              <option value="">Seguidores</option>
-              <option value="">Seguidores</option>
-              <option value="">Seguidores</option>
-              <option value="">Seguidores</option>
-              <option value="">Seguidores</option>
-            </select>
-          </li>
+       
+        <Select title='Genero'/>
+         <Select title='Popularidad'/>
+         <Select title='Seguidores'/>
+
           <li className="filters__item">
             <label>
               <input className="filters__input" type="radio" name="favorite" />
