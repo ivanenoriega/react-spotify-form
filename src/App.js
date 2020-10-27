785f2a26645c02cdcda77787930c02caecb0da28
damiloyola <damiloyola@gmail.com>
2020-10-26 21:54:48 -0300

    props desde array artistsList a component ArtistCard

--- src/App.js
+++ src/App.js
@@ -8,30 +8,38 @@ import Search from "./components/Search";
 import "./App.css";
 
 function App() {
-  const artists = [];
+    const artists = [];
 
-  for (let i = 0; i < artistsList.length; i++) {
-    artists.push(<ArtistCard />);
-  }
+    for (let i = 0; i < artistsList.length; i++) {
+        artists.push(
+            <ArtistCard
+                liked={false}
+                url={artistsList[i].images[2].url}
+                name={artistsList[i].name}
+                followers={artistsList[i].followers.total}
+                stars={artistsList[i].popularity}
+            />
+        );
+    }
 
-  return (
-    <div className="App">
-      <header className="head">
-        <img className="head__logo" src={logo} alt="Spotify logo" />
-        <h1 className="head__title">Artists Search Form - Test</h1>
-      </header>
-      <Nav>
-        <Search />
-        <Select title="Genero" />
-        <Select title="Popularidad" />
-        <Select title="Seguidores" />
-        <Favorite />
-      </Nav>
-      <section className="artists">
-        <ul className="artists__list">{artists}</ul>
-      </section>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="head">
+                <img className="head__logo" src={logo} alt="Spotify logo" />
+                <h1 className="head__title">Artists Search Form - Test</h1>
+            </header>
+            <Nav>
+                <Search />
+                <Select title="Genero" />
+                <Select title="Popularidad" />
+                <Select title="Seguidores" />
+                <Favorite />
+            </Nav>
+            <section className="artists">
+                <ul className="artists__list">{artists}</ul>
+            </section>
+        </div>
+    );
 }
 
 export default App;
