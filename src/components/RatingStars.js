bcfdd7ce330b5297e457a0d863582bfc1b5a13ce
Jesus Lencina <jesusignaciolencina@gmail.com>
2020-10-26 21:53:05 -0300

    Update RatingStars.js

--- src/components/RatingStars.js
+++ src/components/RatingStars.js
@@ -5,6 +5,37 @@ class RatingStars extends React.Component {
   determinePopularity = () => {};
 
   render() {
+    this.props = 68;
+
+    //DETERMINE AMOUNT OF STARS
+
+    let amount;
+
+    switch (this.props) {
+      case this.props > 20:
+        amount = 1;
+        break;
+
+      case this.props > 40:
+        amount = 2;
+        break;
+
+      case this.props > 60:
+        amount = 3;
+        break;
+
+      case this.props > 80:
+        amount = 4;
+        break;
+
+      case this.props <= 80:
+        amount = 5;
+        break;
+
+      default:
+        break;
+    }
+
     return (
       <div className="artist__stars">
         <Star className="artist__star" />
